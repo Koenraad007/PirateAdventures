83efdc32aebee3737232d4979d1ef67d85447e46
KoenVA <koenvanaken1999@gmail.com>
2024-08-17 19:50:40 +0200

    Make IGameObject interface

---
+++ PirateAdventures/Interfaces/IGameObject.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PirateAdventures.Interfaces
+{
+    public interface IGameObject
+    {
+        void Update();
+
+        void Draw();
+    }
+}
