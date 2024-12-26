429211454c37218e511a17c92517215dec94a1ed
KoenVA <koenvanaken1999@gmail.com>
2024-12-26 19:22:42 +0100

    Add GameState Enum

--- PirateAdventures/obj/Debug/net6.0-windows/PirateAdventures.AssemblyInfo.cs
+++ PirateAdventures/obj/Debug/net6.0-windows/PirateAdventures.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PirateAdventures")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e14c40e2296d062d052640f475d523fea4250974")]
 [assembly: System.Reflection.AssemblyProductAttribute("PirateAdventures")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PirateAdventures")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
